65f709c74813cc1249647b72efaabd4a7be94200
eroauditore <alfa.fenrir@gmail.com>
2021-09-15 11:58:18 -0500

    Factura estatus actualizado

--- src/components/Common/FacturaEstatus.jsx
+++ src/components/Common/FacturaEstatus.jsx
@@ -1,40 +1,40 @@
-import React from "react";
+import React from 'react';
 
 const FacturaEstatus = ({ data }) => {
   /* if (!data) return <div></div>;*/
 
-  let renderText = "";
-  let renderClass = "";
+  let renderText = '';
+  let renderClass = '';
   switch (data) {
-    case "0": {
-      renderText = "SOLICITADA";
-      renderClass = "badge bg-secondary";
+    case '0': {
+      renderText = 'SOLICITADA';
+      renderClass = 'badge bg-secondary';
       break;
     }
-    case "1": {
-      renderText = "EN PROCESO";
-      renderClass = "badge bg-warning";
+    case '1': {
+      renderText = 'EN PROCESO';
+      renderClass = 'badge bg-warning';
       break;
     }
-    case "2": {
-      renderText = "EMITIDA";
-      renderClass = "badge bg-success";
+    case '2': {
+      renderText = 'EMITIDA';
+      renderClass = 'badge bg-success';
       break;
     }
-    case "generado": {
-      renderText = "PAGADA";
-      renderClass = "badge bg-success";
+    case 'generado': {
+      renderText = 'PAGADA';
+      renderClass = 'badge bg-success';
       break;
     }
 
-    case "4": {
-      renderText = "CANCELADA";
-      renderClass = "badge bg-danger";
+    case '4': {
+      renderText = 'CANCELADA';
+      renderClass = 'badge bg-danger';
       break;
     }
     default: {
-      renderText = "PENDIENTE";
-      renderClass = "badge bg-secondary";
+      renderText = 'PENDIENTE';
+      renderClass = 'badge bg-secondary';
       break;
     }
   }
